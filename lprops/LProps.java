f1004d5a639d049867f68ea43668cc0f881205c0
Thomas Brand <tom@trellis.ch>
2016-03-06 07:09:05 +0100

    add methods getOrderedProperties, get, set, print. all static

--- lprops/LProps.java
+++ lprops/LProps.java
@@ -1,33 +1,26 @@
 //http://docs.oracle.com/javase/tutorial/reflect/member/fieldTypes.html
 //tb/160306
 import java.util.Properties;
+import java.util.Enumeration;
+import java.util.TreeSet;
+import java.util.Collections;
 import java.io.InputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.lang.reflect.Field;
 //========================================================================
 //========================================================================
 class LProps
 {
 	public LProps(){}
+
 //========================================================================
 	public static boolean load(String configfile_uri, Object configurable_object)
 	{
-		Properties props=new Properties();
-		InputStream is=null;
-		try 
+		try
 		{
-			File f=new File(configfile_uri);
-			if(!f.exists() || !f.canRead())
-			{
-				return false;
-			}
-			is=new FileInputStream(f);
-			if(is==null)
-			{
-				return false;
-			}
-			props.load(is);
+			Properties props=checkLoadFile(configfile_uri);
 			if(props==null)
 			{
 				return false;
@@ -77,4 +70,98 @@ class LProps
 		}
 		return false;
 	}//end load()
+
+//========================================================================
+	public static Properties checkLoadFile(String configfile_uri)
+	{
+		Properties props=new Properties();
+		InputStream is=null;
+		try
+		{
+			File f=new File(configfile_uri);
+			if(!f.exists() || !f.canRead())
+			{
+				return null;
+			}
+			is=new FileInputStream(f);
+			if(is==null)
+			{
+				return null;
+			}
+			props.load(is);
+			if(props==null)
+			{
+				return null;
+			}
+			is.close(); ////
+			return props;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+//========================================================================
+	public static Properties getOrderedProperties(Properties props)
+	{
+		//http://stackoverflow.com/questions/17011108/how-can-i-write-java-properties-in-a-defined-order
+		Properties tmp=new Properties()
+		{
+			public synchronized Enumeration<Object> keys()
+			{
+				return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+			}
+		};
+		tmp.putAll(props);
+		return tmp;
+	}
+
+//========================================================================
+	public static Object get(String configfile_uri, String key)
+	{
+		Properties props=checkLoadFile(configfile_uri);
+		if(props==null){return null;}
+		try
+		{
+			return props.getProperty(key);	
+		}
+		catch(Exception e){e.printStackTrace();}
+		return null;
+	}
+
+//========================================================================
+	public static boolean set(String configfile_uri, String key, Object val)
+	{
+		Properties props=checkLoadFile(configfile_uri);
+		if(props==null){return false;}
+		try
+		{
+			props.setProperty(key, ""+val);
+//			getOrderedProperties(props).store(System.out, null);
+			///OVERWRITE ORIGINAL FILE. LOOSING ALL COMMENTS. KEYS IN ALPHABETIC ORDER.
+			getOrderedProperties(props).store(new FileOutputStream(new File(configfile_uri)), null);
+//			System.out.println(key+"="+get(configfile_uri,key));
+//			print(configfile_uri);
+			return true;
+		}
+		catch(Exception e){e.printStackTrace();}
+		return false;
+	}
+
+//========================================================================
+	public static boolean print(String configfile_uri)
+	{
+		Properties props=checkLoadFile(configfile_uri);
+		if(props==null){return false;}
+		try
+		{
+			getOrderedProperties(props).store(System.out, null);
+			return true;
+		}
+		catch(Exception e){e.printStackTrace();}
+		return false;
+	}
+
 }//end class LProps
