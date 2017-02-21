a63eba4f93ccef5ac06c070318876c2d1c12e86d
Thomas Brand <tom@trellis.ch>
2017-02-21 04:05:57 +0100

    add dump/write support for java.util.Vector. READ and WRITE properties files using UTF-8 character encoding.

--- lprops/LPropsTest.java
+++ lprops/LPropsTest.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 //========================================================================
 //========================================================================
 class LPropsTest
@@ -6,9 +8,10 @@ class LPropsTest
 	public long	al=24;
 	public float 	af=78.90f;
 	public double	ad=79.909090909d;
-	public String 	as="hallo velo";
+	public String 	as="hallo velo ☕ x";
 	public char	ac='@';
 	public boolean 	ab=false;
+	public Vector	av=new Vector();
 
 	private int	apriv=0;
 
@@ -22,63 +25,81 @@ class LPropsTest
 		}
 		new LPropsTest(args);
 	}
+
 //========================================================================
-	public LPropsTest(String[] args)
+	public static void printVector(Vector v)
 	{
-		System.err.println("values before load:");
-		//member variables after instance created
+		for(int i=0;i<v.size();i++)
+		{
+			System.err.println("av["+i+"]="+v.get(i));
+		}
+	}
+
+//========================================================================
+	public void printCurrentValues()
+	{
 		System.err.println("ai="+ai);
 		System.err.println("al="+al);
 		System.err.println("af="+af);
 		System.err.println("ad="+ad);
-		System.err.println("as="+as);
+		System.err.println("as="+as+".EOL");
 		System.err.println("ac="+ac);
 		System.err.println("ab="+ab);
-		System.err.println("apriv="+apriv);
+		printVector(av);
 
-		System.err.println("\nvalues found by dumpObject:");
+		System.err.println("apriv="+apriv);
+	}
+
+//========================================================================
+	public LPropsTest(String[] args)
+	{
+		//adding initial values to vector
+		//values from any properties file(s) will be *added* to the *existing* tokens
+		//(the vector is not reset if av= keys are found)
+		//the user of LProps has to take care of removing double entries, re-order etc.
+		av.add("foo");
+		av.add("bar");
+
+		//LProps will always put string tokens to a vector.
+		//the user of LProps has to take care of the destination datatype, for instance
+		//converting to integer or float.
+
+		System.err.println("values before loading properties from file (hardcoded):");
+		//member variables after instance created
+		printCurrentValues();
+
+		System.err.println("\nvalues found by dumpObject (test private visibility):");
 		System.err.println(LProps.dumpObject(this));
 
-		System.err.println("storing object members to file a.properties");
+		System.err.println("storing current public object member key=value pairs to file a.properties");
 		LProps.store("a.properties",this);
+
+		System.err.println("\nprint from file"+args[0]+":");
+		LProps.print(args[0]);
+
+		System.err.println("\nloading argument properties file "+args[0]);
 
 		if(!LProps.load(args[0],this))
 		{
 			System.err.println("could not load properties file "+args[0]);
 		}
 
-		System.err.println("\nvalues after load:");
-
+		System.err.println("\nvalues after loading "+args[0]+":");
 		//member variables afer propertis file loaded
-		System.err.println("ai="+ai);
-		System.err.println("al="+al);
-		System.err.println("af="+af);
-		System.err.println("ad="+ad);
-		System.err.println("as="+as);
-		System.err.println("ac="+ac);
-		System.err.println("ab="+ab);
-		System.err.println("apriv="+apriv);
+		printCurrentValues();
 
 		System.err.println("\ntesting LPropsMan:");
+		System.err.println("\ntrying to load 'a.properties', 'b.properties', '~/.na.prop'...");
 
 		LPropsMan p=new LPropsMan();
 		p.add("a.properties");//current directory
 		p.add("b.properties");
 		p.add(System.getProperty("user.home")+"/.na.prop");
 		p.load(this);
-		System.err.println("\nafter overloading:");
-		System.err.println(LProps.dumpObject(this));
+
+		System.err.println("\nafter overloading a.properties < b.properties < ~/.na.prop:");
+//		System.err.println(LProps.dumpObject(this));
+		printCurrentValues();
 	}//end constructor
 }//end class LPropsTest
-
-/*
-ai=23
-al=24
-af=78.9
-ad=79.909090909
-as=hallo velo
-ac=@
-ab=false
-apriv=99
-*/
 //EOF
--- lprops/make.sh
+++ lprops/make.sh
@@ -9,32 +9,19 @@ ad=99.909090909
 as=foo
 ac=\`
 ab=true
+av=hutse,fluts
 apriv=99
-lprops_pre_load_dump=my label
+lprops_pre_load_dump=a label for a setting-driven (lprops_pre_load_dump=) pre-load config dump
+lprops_post_load_dump=a label for a setting-driven (lprops_post_load_dump=) post-load config dump
 _EOF_
 
-javac -source 1.6 -target 1.6 *.java && java LPropsTest "$tmpfile"
+javac -source 1.6 -target 1.6 *.java
+
+echo "calling LPropsTest with properties file $tmpfile ..."
+cat "$tmpfile"
+
+java LPropsTest "$tmpfile"
 
 rm -f "$tmfile"
 
 exit
-###########
-values before load:
-ai=23
-al=24
-af=78.9
-ad=79.909090909
-as=hallo velo
-ac=@
-ab=false
-apriv=0
-
-values after load:
-ai=66
-al=77
-af=88.9
-ad=99.909090909
-as=foo
-ac=`
-ab=true
-apriv=0
